fc4262937a1480e1b9533d721ba3e5ee4d03d5ae
Lola <walaa.elsafadi@gmail.com>
2024-09-22 19:33:13 +0200

    src

--- src/input_handler_utils.c
+++ src/input_handler_utils.c
@@ -6,7 +6,7 @@
 /*   By: wel-safa <wel-safa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 20:54:36 by wel-safa          #+#    #+#             */
-/*   Updated: 2024/09/22 18:42:18 by wel-safa         ###   ########.fr       */
+/*   Updated: 2024/09/22 19:31:58 by wel-safa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int	carrotcount(t_state *state, int start)
 	end = start;
 	while (state->input[end] == c)
 		end++;
-	if (end - start > 2) // three or more carrots // error, handle and cleanup
+	if (end - start > 2)
 		return (3);
 	else if (end - start == 2)
-		return (2); // two carrots
-	return (1); // only one carrot
+		return (2);
+	return (1);
 }
 
 /*takes index of start of word i
@@ -39,8 +39,6 @@ int	find_word_end(t_state *state, int i)
 {
 	char	c;
 
-	if (!state || !state->input) // Check if state and input are valid from chatgpt
-		return (-1);
 	while (state->input[i])
 	{
 		c = state->input[i];
@@ -48,18 +46,14 @@ int	find_word_end(t_state *state, int i)
 		{
 			i = find_closed_quote(state, i) + 1;
 			if (i < 0)
-				return (-1); // unclosed quote
+				return (-1);
 			c = state->input[i];
 		}
 		if (c == '|' || c == ' ' || c == '>' || c == '<' || c == 0)
-		{
-			// end of file name or word
-			// check for restrictions of file name
-			return (i - 1); // return end of file name or word
-		}
+			return (i - 1);
 		i++;
 	}
-	return (i - 1); // end of string, returns index before NULL
+	return (i - 1);
 }
 
 /*takes index i of start of quotes
@@ -79,9 +73,6 @@ int	find_closed_quote(t_state *state, int i)
 			return (i);
 		i++;
 	}
-	// unclosed quote detected
-	// error
-	// cleanup
 	write(2, "syntax error unclosed quotes\n", 29);
 	return (-2);
 }
@@ -93,9 +84,6 @@ void	create_word(t_state *state, int start, int end)
 {
 	t_list	*newword;
 
-	//printf("start = %i\n", start);
-	//printf("end = %i\n", end);
-	// creating new word
 	newword = (t_list *)malloc(sizeof(t_list));
 	if (!newword)
 		error_exit(state);
@@ -103,7 +91,6 @@ void	create_word(t_state *state, int start, int end)
 	if (!newword->content)
 		error_exit(state);
 	ft_strlcpy(newword->content, state->input + start, end - start + 2);
-	//printf("%s\n", (char *)(newword->content));
 	newword->next = NULL;
 	ft_lstadd_back(&state->words, newword);
 }
