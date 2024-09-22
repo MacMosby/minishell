3ec040e9f79cacd9a7dd679e62a9522470d6bfc2
Lola <walaa.elsafadi@gmail.com>
2024-09-22 22:25:09 +0200

    norm

--- src/splitting.c
+++ src/splitting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   splitting.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wel-safa <wel-safa@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: wel-safa <wel-safa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 23:39:02 by wel-safa          #+#    #+#             */
-/*   Updated: 2024/08/20 23:31:07 by wel-safa         ###   ########.fr       */
+/*   Updated: 2024/09/22 22:24:37 by wel-safa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,34 @@
 /*takes start index s and end index e and
 string pointer word and t_list double pointer newwordlist
 it creates new word from index s to e of char **word
-and adds it to newwordlist*/
-void	split_words_create(t_state *state, int s, int e, char **word, t_list **newwordlist)
+and adds it to newwordlist
+first if statement is true if space is at beg or end of word*/
+int	split_words_create(int s, int e, char **word, t_list **newwordlist)
 {
 	t_list	*newword;
 
 	if (s > ((int)ft_strlen(*word) - 1) || e < 0)
-		// happens when space at beginning or end of word
-		return ;
+		return (0);
 	newword = (t_list *)malloc(sizeof(t_list));
 	if (!newword)
-		error_exit(state);
+		return (-1);
 	newword->content = (char *)malloc((e - s + 2) * sizeof(char));
 	if (!newword->content)
-		error_exit(state);
+		return (-1);
 	ft_strlcpy(newword->content, *word + s, e - s + 2);
 	newword->next = NULL;
 	ft_lstadd_back(newwordlist, newword);
+	return (0);
 }
 
-/*takes string pointer word and t_list double pointer for newwordlist
-if it finds a space not inside quotes, it splits word at that index into two words
+/*takes string pointer word and t_list double pointer for newwordlist nwl
+if it finds a space not inside quotes, it splits word at that index 
+into two words
 adds them to list newwordlist
 if space is at beginning or end of word,
 it will return a list of one word removing the space
 */
-void	split_words(t_state *state, char **word, t_list **newwordlist)
+void	split_words(t_state *state, char **word, t_list **nwl)
 {
 	int		sq_flag;
 	int		dq_flag;
@@ -57,12 +59,27 @@ void	split_words(t_state *state, char **word, t_list **newwordlist)
 			dq_flag = !dq_flag;
 		if ((*word)[i] == ' ' && !sq_flag && !dq_flag)
 		{
-			split_words_create(state, 0, i - 1, word, newwordlist);
-			split_words_create(state, i + 1, ((int)ft_strlen(*word)) - 1, word, newwordlist);
+			if (split_words_create(0, i - 1, word, nwl) < 0)
+				error_exit(state);
+			if (split_words_create(i + 1, 
+					((int)ft_strlen(*word)) - 1, word, nwl) < 0)
+				error_exit(state);
 			break ;
 		}
 		i++;
 	}
+}
+
+void	insert_nwl(t_list *bef, t_list *aft, t_list *nwl, t_node *cmd_content)
+{
+	if (bef)
+		bef->next = nwl;
+	else
+		cmd_content->words = nwl;
+	if (nwl->next)
+		nwl->next->next = aft;
+	else
+		nwl->next = aft;
 }
 
 /*splitting words after expansion: it iterates over list of words
@@ -72,14 +89,12 @@ in place of the one word in the t_list words in t_node cmd_content,
 it then loops again on second replaced word*/
 void	splitting_cmd_words(t_state *state, t_node *cmd_content)
 {
-	//printf("\nsplitting...\n");
 	t_list	*word;
 	t_list	*before;
 	t_list	*after;
 	t_list	*newwordlist;
 
 	word = cmd_content->words;
-	newwordlist = NULL;
 	before = NULL;
 	while (word)
 	{
@@ -88,16 +103,9 @@ void	splitting_cmd_words(t_state *state, t_node *cmd_content)
 		split_words(state, (char **) &(word->content), &newwordlist);
 		if (newwordlist)
 		{
-			if (before)
-				before->next = newwordlist;
-			else
-				cmd_content->words = newwordlist;
-			if (newwordlist->next)
-				newwordlist->next->next = after;
-			else
-				newwordlist->next = after;
+			insert_nwl(before, after, newwordlist, cmd_content);
 			ft_lstdelone(word, free);
-			word = newwordlist->next;
+			word = newwordlist;
 		}
 		else
 		{
